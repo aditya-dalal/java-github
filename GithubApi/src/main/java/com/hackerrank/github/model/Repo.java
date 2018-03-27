0786ddfdcb6562e9e7ad275a647704ffdc549605
Aditya Dalal <aditya.dalal@flipkart.com>
2018-03-27 07:04:47 +0530

    fixed expected output in http04.json for actors/streak api

--- GithubApi/src/main/java/com/hackerrank/github/model/Repo.java
+++ GithubApi/src/main/java/com/hackerrank/github/model/Repo.java
@@ -3,10 +3,7 @@ package com.hackerrank.github.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 @Setter
